7b04b1060355de888096faf49d5aaa7b3007fd6a
GKaszewski <gabrielkaszewski@gmail.com>
2018-11-28 22:21:41 +0100

    Added keywords to search query fixed

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 import urllib.parse, requests, json, webbrowser, random
 
 main_api = 'https://images-api.nasa.gov/search?'
+cool_phenomena = ['nebula','orion','blazars','milky way','jupiter', 'Aurora', 'Sun', 'NuSTAR', 'solar flares', 'Galaxy Evolution Explorer GALEX', 'coronal mass ejection', 'ISS', 'KSC', 'star']
 
-keyword=input('Type a keword: ')
-#print(keyword)
-url = main_api + urllib.parse.urlencode({'q':keyword,'media_type':'image'})
-json_data = requests.get(url).json()
-rndPhoto = random.randint(0, len(json_data['collection']['items']));
-picture_collection = json_data['collection']['items'][rndPhoto]['href']
-picture_data = requests.get(picture_collection).json()
-picture_url = picture_data[0]
-#print(picture_url)
-webbrowser.open_new_tab(picture_url)
+def getCoolPicture():
+    keywordID = random.randint(0, len(cool_phenomena))
+    keyword = cool_phenomena[keywordID]
+    print(keyword)
+    url = main_api + urllib.parse.urlencode({'q':keyword,'media_type':'image'})
+    json_data = requests.get(url).json()
+    rndPhoto = random.randint(0, len(json_data['collection']['items']));
+    picture_collection = json_data['collection']['items'][rndPhoto]['href']
+    picture_data = requests.get(picture_collection).json()
+    picture_url = picture_data[0]
+    #print(picture_url)
+    webbrowser.open_new_tab(picture_url)
+
+getCoolPicture();
